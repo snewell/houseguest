63e6d61ddc16b5f9b605994e405fd254046ce61a
Stephen Newell <sjnewell@gmail.com>
2018-04-02 20:00:39 -0600

    Run clang-format

--- test_synchronize.cpp
+++ test_synchronize.cpp
@@ -5,17 +5,14 @@
 TEST(Synchronize, simple)
 {
     std::mutex m;
-    houseguest::synchronize(m, [&m]() {
-        ASSERT_FALSE(m.try_lock());
-    });
+    houseguest::synchronize(m, [&m]() { ASSERT_FALSE(m.try_lock()); });
 }
 
 TEST(Synchronize, simple_forward)
 {
     std::mutex m;
-    houseguest::synchronize(m, [](std::mutex &inner) {
-        ASSERT_FALSE(inner.try_lock());
-    }, m);
+    houseguest::synchronize(
+        m, [](std::mutex & inner) { ASSERT_FALSE(inner.try_lock()); }, m);
 }
 
 TEST(Synchronize, unique)
@@ -30,10 +27,13 @@ TEST(Synchronize, unique)
 TEST(Synchronize, unique_forward)
 {
     std::mutex m;
-    houseguest::synchronize_unique(m, [](std::unique_lock<std::mutex> lock, std::mutex &inner) {
-        ASSERT_TRUE(lock.owns_lock());
-        ASSERT_FALSE(inner.try_lock());
-    }, m);
+    houseguest::synchronize_unique(
+        m,
+        [](std::unique_lock<std::mutex> lock, std::mutex & inner) {
+            ASSERT_TRUE(lock.owns_lock());
+            ASSERT_FALSE(inner.try_lock());
+        },
+        m);
 }
 
 TEST(Synchronize, make_synchronize)
@@ -53,10 +53,11 @@ TEST(Synchronize, make_synchronize)
 TEST(Synchronize, make_synchronize_forward)
 {
     std::mutex m;
-    auto sync_fn = houseguest::make_synchronize(m, [](std::mutex &inner, bool & called) {
-        ASSERT_FALSE(inner.try_lock());
-        called = true;
-    });
+    auto sync_fn =
+        houseguest::make_synchronize(m, [](std::mutex & inner, bool & called) {
+            ASSERT_FALSE(inner.try_lock());
+            called = true;
+        });
     ASSERT_TRUE(m.try_lock());
     m.unlock();
     auto called = false;
@@ -68,11 +69,12 @@ TEST(Synchronize, make_synchronize_unique)
 {
     auto called = false;
     std::mutex m;
-    auto sync_fn = houseguest::make_synchronize_unique(m, [&m, &called](std::unique_lock<std::mutex> lock) {
-        ASSERT_TRUE(lock.owns_lock());
-        ASSERT_FALSE(m.try_lock());
-        called = true;
-    });
+    auto sync_fn = houseguest::make_synchronize_unique(
+        m, [&m, &called](std::unique_lock<std::mutex> lock) {
+            ASSERT_TRUE(lock.owns_lock());
+            ASSERT_FALSE(m.try_lock());
+            called = true;
+        });
     ASSERT_TRUE(m.try_lock());
     m.unlock();
     sync_fn();
@@ -82,11 +84,13 @@ TEST(Synchronize, make_synchronize_unique)
 TEST(Synchronize, make_synchronize_unique_forward)
 {
     std::mutex m;
-    auto sync_fn = houseguest::make_synchronize_unique(m, [](std::unique_lock<std::mutex> lock, std::mutex &inner, bool & called) {
-        ASSERT_TRUE(lock.owns_lock());
-        ASSERT_FALSE(inner.try_lock());
-        called = true;
-    });
+    auto sync_fn = houseguest::make_synchronize_unique(
+        m, [](std::unique_lock<std::mutex> lock, std::mutex & inner,
+              bool & called) {
+            ASSERT_TRUE(lock.owns_lock());
+            ASSERT_FALSE(inner.try_lock());
+            called = true;
+        });
     ASSERT_TRUE(m.try_lock());
     m.unlock();
     auto called = false;
