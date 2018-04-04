a866d5abf835e9d27aa7b475024de3f77c709203
Stephen Newell <sjnewell@gmail.com>
2018-04-03 23:07:10 -0600

    Document mutex.hpp and thread_safe_object.hpp

--- include/houseguest/mutex.hpp
+++ include/houseguest/mutex.hpp
@@ -7,26 +7,66 @@
 #include <shared_mutex>
 #endif
 
+/** \file
+ *
+ * \brief Provide a method for specialization of mutex-related types
+ *
+ * The types in this file follow the pattern set by type_traits.  Each struct
+ * provides contains a type alias to the appropriate type.  Default
+ * implementations will point to types provided by the standard library when
+ * possible, but specializations can be provided as needed in client code.
+ *
+ * Convenience types (e.g., lock_guard_t) are provided as well.
+ */
+
 namespace houseguest
 {
+    /** \brief A type that mimics std::lock_guard
+     *
+     * lock_guard is expected to lock a mutex in the constructor and unlock in
+     * its destructor.
+     *
+     * \tparam MUTEX The mutex to specialize for
+     */
     template <typename MUTEX>
     struct lock_guard
     {
         using type = std::lock_guard<MUTEX>;
     };
 
+    /// \brief A convenience type for working with lock_guard
     template <typename MUTEX>
     using lock_guard_t = typename lock_guard<MUTEX>::type;
 
+    /** \brief a type that mimics std::unique_lock
+     *
+     * Unlike lock_guard, a unique_lock can be unlocked without being
+     * destroyed.  This means htey can work with types similar to
+     * std::condition_variable.
+     *
+     * \tparam MUTEX The mutex to spcialize for
+     */
     template <typename MUTEX>
     struct unique_lock
     {
         using type = std::unique_lock<MUTEX>;
     };
 
+    /// \brief A convenience type for working with unique_lock
     template <typename MUTEX>
     using unique_lock_t = typename unique_lock<MUTEX>::type;
 
+    /** \brief A type that mimics std::shared_lock
+     *
+     * A shared_lock can be locked by multiple threads at the same time.  It's
+     * primarily used for scenarios like readers/writers, where many threads
+     * will be readers (shared_lock) but only one can be a writer
+     * (unique_lock).
+     *
+     * \tparam MUTEX The mutex type to specialize for
+     *
+     * \note C++14 does not provide a version of shared_lock.
+     */
     template <typename MUTEX>
     struct shared_lock
 #if __cplusplus == 201703L
@@ -36,6 +76,7 @@ namespace houseguest
 #endif
     ;
 
+    /// \brief A convenience type for working with shared_lock
     template <typename MUTEX>
     using shared_lock_t = typename shared_lock<MUTEX>::type;
 } // namespace houseguest
--- include/houseguest/thread_safe_object.hpp
+++ include/houseguest/thread_safe_object.hpp
@@ -1,29 +1,65 @@
 #ifndef HOUSEGUEST_THREAD_SAFE_OBJECT_HPP
 #define HOUSEGUEST_THREAD_SAFE_OBJECT_HPP 1
 
+#include <cassert>
+
 #include <houseguest/mutex.hpp>
 
 namespace houseguest
 {
+    /** \brief A class to provide access to a mutable object
+     *
+     * A write_handle provides mutable access to some object while holding a
+     * lock.
+     *
+     * \tparam T     The type being managed
+     * \tparam MUTEX The mutex being used to control access
+     *
+     * \note Because write_handles hold a lock during their entire lifetime,
+     *       their scope should be as limited as possible.  Avoid using them in
+     *       any fashion that conflicts with this, including as member
+     *       variables in classes.
+     */
     template <typename T, typename MUTEX>
-    class write_handle
+    class
+#if __cplusplus >= 201703L
+    [[nodiscard]]
+#endif
+    write_handle
     {
     public:
+        /// \brief The lock required by write_handle
         using lock_type = houseguest::unique_lock_t<MUTEX>;
 
+        /** \brief Construct a write handle
+         *
+         * \param t    The object to manage
+         * \param lock A lock that provides exclusive access to \a t
+         */
         write_handle(T & t, lock_type lock)
           : _t{t}
           , _lock{std::move(lock)}
         {
+            assert(_lock.owns_lock());
         }
 
+        /** \brief Retreive a reference to the managed object
+         *
+         * \return A reference to the managed object
+         */
         T & operator*() noexcept
         {
+            assert(_lock.owns_lock());
             return _t;
         }
 
+        /** \brief Retrieve a pointer to the managed object
+         *
+         * \return A pointer to the managed object
+         */
         T * operator->() noexcept
         {
+            assert(_lock.owns_lock());
             return &_t;
         }
 
@@ -32,25 +68,61 @@ namespace houseguest
         lock_type _lock;
     };
 
+    /** \brief A class to provide access to an immutable object
+     *
+     * A read_handle provides immutable access to some object while holding a
+     * lock.  Multiple read_handles to the same object can exist, but the
+     * existance of a single read_handle excludes the ability to create a
+     * write_handle to the same object.
+     *
+     * \tparam T     The type being managed
+     * \tparam MUTEX The mutex being used to control access
+     *
+     * \note Because read_handles hold a lock during their entire lifetime,
+     *       their scope should be as limited as possible.  Avoid using them in
+     *       any fashion that conflicts with this, including as member
+     *       variables in classes.
+     */
     template <typename T, typename MUTEX>
-    class read_handle
+    class
+#if __cplusplus >= 201703L
+    [[nodiscard]]
+#endif
+    read_handle
     {
     public:
+        /// \brief The lock required by read_handle
         using lock_type = houseguest::shared_lock_t<MUTEX>;
 
+        /** \brief Construct a read_handle
+         *
+         * \param t    The object to manage
+         * \param lock The lock to manage access to \a t
+         */
         read_handle(T const & t, lock_type lock)
           : _t{t}
           , _lock{std::move(lock)}
         {
+            assert(_lock.owns_lock());
         }
 
+        /** \brief Retreive a reference to the managed object
+         *
+         * \return A reference to the managed object
+         */
         T const & operator*() const noexcept
         {
+            assert(_lock.owns_lock());
             return _t;
         }
 
+        /** \brief Retreive a pointer to the managed object
+         *
+         * \return A pointer to the managed object
+         */
         T const * operator->() const noexcept
         {
+            assert(_lock.owns_lock());
             return &_t;
         }
 
@@ -59,26 +131,64 @@ namespace houseguest
         lock_type _lock;
     };
 
+    /** \brief Provide a thread-safe wrapper around types
+     *
+     * threadsafe_object provides a method of ensuring an object has the
+     * minimum required lock.
+     *
+     * \tparam T     The type to manage.
+     * \tparam MUTEX The mutex to handle locking.  This type must support both
+     *               unique and shared locks.
+     */
     template <typename T, typename MUTEX = std::shared_mutex>
     class threadsafe_object
     {
     public:
+        /// \brief The type that provides write access to a \a T
         using write_handle_type = write_handle<T, MUTEX>;
 
+        /// \brief The type that provides read access to a \a T
         using read_handle_type = read_handle<T, MUTEX>;
 
+        /** \brief Construct a threadsafe_object
+         *
+         * \tparam Ts Any extra types passed to the constructor
+         *
+         * \param ts Extra arguments passed to the constructor.  Arguments
+         *           aren't required by threadsafe_object, but may be required
+         *           by T.  If provided, they will be passed to T's constructor
+         *           via std::forward.
+         */
         template <typename... Ts>
         threadsafe_object(Ts &&... ts)
           : _t{std::forward<Ts>(ts)...}
         {
         }
 
+        /** \brief Construct a write_handle for the underlying data
+         *
+         * An object can have at most one write_handle at any given time.  If
+         * anybody calls this function while a write_handle to the managed T
+         * already exists, the call will block until the original write_handle
+         * is destroyed.
+         *
+         * This call may also block if any read_handles exist.
+         *
+         * \return A write_handle to modify the managed T
+         */
         auto write()
         {
             typename write_handle_type::lock_type lock{_m};
             return write_handle_type{_t, std::move(lock)};
         }
 
+        /** \brief Construct a read_handle for the underlying data
+         *
+         * An object can have as many read_handles as it wishes at one time,
+         * provided there are no existing write_handles.
+         *
+         * \return A read_handle to modify the managed T
+         */
         auto read() const
         {
             typename read_handle_type::lock_type lock{_m};
