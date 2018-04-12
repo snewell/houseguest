22669fd962b1b3092e5ae5eb72a9f302c2f06a74
Stephen Newell <sjnewell@gmail.com>
2018-04-12 00:02:20 -0600

    Silence clang-tidy warnings generated by gtest

--- ranged_value_test.cpp
+++ ranged_value_test.cpp
@@ -9,29 +9,29 @@ namespace
     using fifties_int = houseguest::ranged_value<int, 50, 60>;
 }
 
-TEST(RangedValue, good_min)
+TEST(RangedValue, good_min) // NOLINT
 {
     two_digit_int tdi{10};
     ASSERT_EQ(10, tdi);
 }
 
-TEST(RangedValue, good_max)
+TEST(RangedValue, good_max) // NOLINT
 {
     two_digit_int tdi{99};
     ASSERT_EQ(99, tdi);
 }
 
-TEST(RangedValue, min_edge)
+TEST(RangedValue, min_edge) // NOLINT
 {
     ASSERT_THROW(two_digit_int{9}, std::out_of_range);
 }
 
-TEST(RangedValue, max_edge)
+TEST(RangedValue, max_edge) // NOLINT
 {
     ASSERT_THROW(two_digit_int{100}, std::out_of_range);
 }
 
-TEST(RangedValue, good_convert)
+TEST(RangedValue, good_convert) // NOLINT
 {
     two_digit_int tdi{55};
     fifties_int fi{tdi};
@@ -39,7 +39,7 @@ TEST(RangedValue, good_convert)
     ASSERT_EQ(tdi, fi);
 }
 
-TEST(RangedValue, bad_convert)
+TEST(RangedValue, bad_convert) // NOLINT
 {
     two_digit_int tdi{65};
     ASSERT_THROW(fifties_int fi{tdi}, std::out_of_range);
@@ -47,14 +47,14 @@ TEST(RangedValue, bad_convert)
 
 // everything after this should fail to compile due to static_asserts
 #if 0
-TEST(RangedValue, invalid_range)
+TEST(RangedValue, invalid_range) // NOLINT
 {
     houseguest::ranged_value<int, 10, 10> invalid{10};
 }
 #endif
 
 #if 0
-TEST(RangedValue, invalid_conversion_max)
+TEST(RangedValue, invalid_conversion_max) // NOLINT
 {
     houseguest::ranged_value<int, 1, 100> positive{52};
     houseguest::ranged_value<int, -100, 0> negative{positive};
@@ -62,7 +62,7 @@ TEST(RangedValue, invalid_conversion_max)
 #endif
 
 #if 0
-TEST(RangedValue, invalid_conversion_min)
+TEST(RangedValue, invalid_conversion_min) // NOLINT
 {
     houseguest::ranged_value<int, -100, 0> negative{-52};
     houseguest::ranged_value<int, 1, 100> positive{negative};
