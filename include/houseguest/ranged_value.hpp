c7c89f8f07bdcd5c9d9dbeaf8f407ae0ba70c6af
Stephen Newell <sjnewell@gmail.com>
2018-04-16 22:00:30 -0600

    Use std::system_error and std::error_code for ranged_value errors

--- include/houseguest/ranged_value.hpp
+++ include/houseguest/ranged_value.hpp
@@ -1,18 +1,60 @@
 #ifndef HOUSEGUEST_RANGED_VALUE_HPP
 #define HOUSEGUEST_RANGED_VALUE_HPP 1
 
-#include <stdexcept>
+#include <system_error>
 #include <type_traits>
 
 namespace houseguest
 {
+    enum class ranged_value_error
+    {
+        success = 0,
+        below_min,
+        above_max
+    };
+
+    struct ranged_value_error_category : std::error_category
+    {
+        const char * name() const noexcept override
+        {
+            return "houseguest::ranged_value";
+        }
+
+        std::string message(int ev) const override
+        {
+            switch(static_cast<ranged_value_error>(ev))
+            {
+            case ranged_value_error::success:
+                return "success";
+                break;
+
+            case ranged_value_error::below_min:
+                return "value is below the minimum allowed";
+                break;
+
+            case ranged_value_error::above_max:
+                return "value is above the maximum allowed";
+                break;
+
+            default:
+                return "(unknown error)";
+                break;
+            }
+        }
+    };
+
+    std::error_code make_error_code(ranged_value_error error)
+    {
+        static ranged_value_error_category const category_instance{};
+        return std::error_code{static_cast<int>(error), category_instance};
+    }
+
     template <typename T, T MIN, T MAX>
-    class ranged_value
+    struct ranged_value
     {
         static_assert(MIN < MAX, "Out of range");
         static_assert(std::is_integral<T>::value, "T must be integral");
 
-    public:
         using underlying_type = T;
 
         explicit ranged_value(T value)
@@ -23,7 +65,8 @@ namespace houseguest
         }
 
         template <T OTHER_MIN, T OTHER_MAX>
-        explicit ranged_value(ranged_value<T, OTHER_MIN, OTHER_MAX> const &other)
+        explicit ranged_value(
+            ranged_value<T, OTHER_MIN, OTHER_MAX> const & other)
           : _value{static_cast<T>(other)}
         {
             static_assert(MIN < OTHER_MAX, "Out of range");
@@ -33,43 +76,43 @@ namespace houseguest
             validate_max(_value);
         }
 
-        constexpr operator T () const noexcept
+        constexpr operator T() const noexcept
         {
             return _value;
         }
 
-        friend constexpr bool operator == (ranged_value<T, MIN, MAX> const &lhs,
-                                           ranged_value<T, MIN, MAX> const &rhs)
+        friend constexpr bool operator==(ranged_value<T, MIN, MAX> const & lhs,
+                                         ranged_value<T, MIN, MAX> const & rhs)
         {
             return lhs._value == rhs._value;
         }
 
-        friend constexpr bool operator != (ranged_value<T, MIN, MAX> const &lhs,
-                                           ranged_value<T, MIN, MAX> const &rhs)
+        friend constexpr bool operator!=(ranged_value<T, MIN, MAX> const & lhs,
+                                         ranged_value<T, MIN, MAX> const & rhs)
         {
             return lhs._value != rhs._value;
         }
 
-        friend constexpr bool operator < (ranged_value<T, MIN, MAX> const &lhs,
-                                          ranged_value<T, MIN, MAX> const &rhs)
+        friend constexpr bool operator<(ranged_value<T, MIN, MAX> const & lhs,
+                                        ranged_value<T, MIN, MAX> const & rhs)
         {
             return lhs._value < rhs._value;
         }
 
-        friend constexpr bool operator <= (ranged_value<T, MIN, MAX> const &lhs,
-                                           ranged_value<T, MIN, MAX> const &rhs)
+        friend constexpr bool operator<=(ranged_value<T, MIN, MAX> const & lhs,
+                                         ranged_value<T, MIN, MAX> const & rhs)
         {
             return lhs._value <= rhs._value;
         }
 
-        friend constexpr bool operator > (ranged_value<T, MIN, MAX> const &lhs,
-                                          ranged_value<T, MIN, MAX> const &rhs)
+        friend constexpr bool operator>(ranged_value<T, MIN, MAX> const & lhs,
+                                        ranged_value<T, MIN, MAX> const & rhs)
         {
             return lhs._value > rhs._value;
         }
 
-        friend constexpr bool operator >= (ranged_value<T, MIN, MAX> const &lhs,
-                                           ranged_value<T, MIN, MAX> const &rhs)
+        friend constexpr bool operator>=(ranged_value<T, MIN, MAX> const & lhs,
+                                         ranged_value<T, MIN, MAX> const & rhs)
         {
             return lhs._value >= rhs._value;
         }
@@ -81,7 +124,8 @@ namespace houseguest
         {
             if(value < MIN)
             {
-                throw std::out_of_range{"too small"};
+                throw std::system_error{
+                    make_error_code(ranged_value_error::below_min)};
             }
         }
 
@@ -89,10 +133,19 @@ namespace houseguest
         {
             if(value >= MAX)
             {
-                throw std::out_of_range{"too large"};
+                throw std::system_error{
+                    make_error_code(ranged_value_error::above_max)};
             }
         }
     };
-}
+} // namespace houseguest
+
+namespace std
+{
+    template <>
+    struct is_error_code_enum<houseguest::ranged_value_error> : true_type
+    {
+    };
+} // namespace std
 
 #endif
--- ranged_value_test.cpp
+++ ranged_value_test.cpp
@@ -7,7 +7,7 @@ namespace
     using two_digit_int = houseguest::ranged_value<int, 10, 100>;
 
     using fifties_int = houseguest::ranged_value<int, 50, 60>;
-}
+} // namespace
 
 TEST(RangedValue, good_min) // NOLINT
 {
@@ -23,12 +23,30 @@ TEST(RangedValue, good_max) // NOLINT
 
 TEST(RangedValue, min_edge) // NOLINT
 {
-    ASSERT_THROW(two_digit_int{9}, std::out_of_range);
+    try
+    {
+        two_digit_int{9};
+        FAIL();
+    }
+    catch(std::system_error const & se)
+    {
+        ASSERT_EQ(static_cast<int>(houseguest::ranged_value_error::below_min),
+                  se.code().value());
+    }
 }
 
 TEST(RangedValue, max_edge) // NOLINT
 {
-    ASSERT_THROW(two_digit_int{100}, std::out_of_range);
+    try
+    {
+        two_digit_int{100};
+        FAIL();
+    }
+    catch(std::system_error const & se)
+    {
+        ASSERT_EQ(static_cast<int>(houseguest::ranged_value_error::above_max),
+                  se.code().value());
+    }
 }
 
 TEST(RangedValue, good_convert) // NOLINT
@@ -41,8 +59,17 @@ TEST(RangedValue, good_convert) // NOLINT
 
 TEST(RangedValue, bad_convert) // NOLINT
 {
-    two_digit_int tdi{65};
-    ASSERT_THROW(fifties_int fi{tdi}, std::out_of_range);
+    try
+    {
+        two_digit_int tdi{65};
+        fifties_int fi{tdi};
+        FAIL();
+    }
+    catch(std::system_error const & se)
+    {
+        ASSERT_EQ(static_cast<int>(houseguest::ranged_value_error::above_max),
+                  se.code().value());
+    }
 }
 
 // everything after this should fail to compile due to static_asserts
